ddf882d5d64a54557c26fa0522ceb67b344f0c4c
Samuel Sánchez García <samuelsanchezg2003@gmail.com>
2026-07-22 12:55:38 +0200

    BD: sembrar 4 supermercados adicionales en Almería

---
+++ backend/src/db/migrations/004_seed_supermercados_extra.sql
@@ -0,0 +1,5 @@
+INSERT INTO supermercados (nombre, cadena, lat, lng, direccion) VALUES
+  ('Consum Vega de Acá',       'Consum',    36.830332, -2.440774, 'Vega de Acá'),
+  ('Lidl Cortijo Grande',      'Lidl',      36.835419, -2.446474, 'Cortijo Grande'),
+  ('Aldi 500 Viviendas',       'Aldi',      36.835922, -2.447741, '500 Viviendas'),
+  ('Carrefour Grande Almería', 'Carrefour', 36.841310, -2.443796, 'Grande Almería');
